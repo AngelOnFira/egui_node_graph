704dbb4958a97645d63c8ad8761fb6a989f8f346
Forest Anderson <forestkzanderson@gmail.com>
2023-08-02 16:18:14 -0400

    Working on backend

--- egui_node_graph_example/src/main.rs
+++ egui_node_graph_example/src/main.rs
@@ -4,10 +4,28 @@
 
 use egui_node_graph_example::NodeGraphExample;
 
+mod webserver;
+
 // When compiling natively:
 #[cfg(not(target_arch = "wasm32"))]
 fn main() {
     use eframe::egui::Visuals;
+    use tokio::runtime::Runtime;
+
+    use crate::webserver::webserver;
+
+    // Create a message channel for the web server thread
+    let (server_tx, server_rx) = std::sync::mpsc::channel();
+
+    // Start a web server thread
+    std::thread::spawn(move || {
+        let rt = Runtime::new().unwrap();
+
+        // Use the runtime
+        rt.block_on(async {
+            webserver(server_rx).await.unwrap();
+        })
+    });
 
     eframe::run_native(
         "Egui node graph example",
